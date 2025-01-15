8daabbc6a1abc05566283fdf816500522cc18b02
edwardkoikai <edwardkoikai@gmail.com>
2025-01-16 01:12:15 +0300

    feat: update Transaction entity to include additional fields and status enum

--- src/api/mpesa/entities/transaction.entity.ts
+++ src/api/mpesa/entities/transaction.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Status } from '../enums/transaction-status.enum';
 
 @Entity('transactions')
 export class Transaction {
@@ -9,25 +10,34 @@ export class Transaction {
     transactionId: string;
 
     @Column()
-    amount: number;
-
-    @Column()
-    phoneNumber: string;
-
-    @Column()
-    status: string;
-
-    @Column()
     MerchantRequestID: string;
 
     @Column()
     CheckoutRequestID: string;
 
     @Column()
-    resultCode: number;
+    resultCode: string;
 
     @Column()
     resultDesc: string;
+
+    @Column('decimal')
+    amount: number;
+
+    @Column()
+    phoneNumber: string;
+
+    @Column()
+    status: Status
+
+    @Column({ nullable: true })
+    mpesaReceiptNumber: string;
+
+    @Column({ type: 'timestamp', nullable: true })
+    transactionDate: Date;
+
+    @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+    balance: number;
 
     @CreateDateColumn()
     createdAt: Date;
