f80aebbcfa0a61bddfc9da07b9cab2046388e728
edwardkoikai <edwardkoikai@gmail.com>
2025-01-16 01:11:55 +0300

    feat: add 'warn' level to logger in main.ts for improved logging

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true, logger: ['error', 'verbose', 'debug', 'log']});
+  const app = await NestFactory.create(AppModule, {cors: true, logger: ['error', 'verbose', 'debug', 'log', 'warn']});
   
   const config = new DocumentBuilder()
   . setTitle('Safaricom Daraja Api')
