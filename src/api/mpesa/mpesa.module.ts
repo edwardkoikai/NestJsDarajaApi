f4f8472b0658c8b4544189a34dadb616847f0de1
edwardkoikai <edwardkoikai@gmail.com>
2025-01-15 23:14:13 +0300

    feat: update MpesaModule to include AuthService and TypeOrm support for Transaction entity

--- src/api/mpesa/mpesa.module.ts
+++ src/api/mpesa/mpesa.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MpesaService } from './service/mpesa.service';
 import { MpesaController } from './controller/mpesa.controller';
+import { Transaction } from './entities/transaction.entity';
+import { AuthService } from './service/auth.service';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Transaction])],
     controllers: [MpesaController],
-    providers: [MpesaService],
+    providers: [MpesaService, AuthService],
     exports: [MpesaService]
 })
 export class MpesaModule {}
