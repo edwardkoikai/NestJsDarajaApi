bb9be86efc13a50d27855845e6e0a7095cbcd2f6
edwardkoikai <edwardkoikai@gmail.com>
2025-01-15 10:42:53 +0300

    feat: add LoggerMiddleware for request logging and integrate Redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,30 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as dotenv from 'dotenv';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ApiModule } from './api/api.module';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 dotenv.config();
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}), 
+    ConfigModule.forRoot({isGlobal: true}),
+    RedisModule.forRootAsync({
+      useFactory: (configService: ConfigService) => configService.get('REDIS_URL'),
+      inject: [ConfigService],
+  }), 
     DatabaseModule,
     ApiModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
